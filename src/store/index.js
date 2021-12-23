e30ed636143e978ac606549235c93d4a0bb663a4
wen <wen.you@nextfortune.tw>
2021-12-23 18:00:15 +0800

    fix round one rule

--- src/store/index.js
+++ src/store/index.js
@@ -61,11 +61,18 @@ function GameSizeGet(team_count) {
   });
 }
 
+// need fix
+function GameSortGet(gameLen) {
+  const multiple = 2;
+  console.log(gameLen, multiple);
+}
+console.log(GameSortGet(32));
+
 export default createStore({
   state: {
     type: Object.keys(CONTEST_TYPE)[0],
-    teamCount: 10,
-    teamInfo: Array.from({ length: 10 }, (v, i) =>
+    teamCount: 6,
+    teamInfo: Array.from({ length: 6 }, (v, i) =>
       Object.assign(
         {},
         { ...TEAM_FORM, id: i.toString(), is_seed: i % 4 === 0 }
@@ -96,20 +103,15 @@ export default createStore({
       state.teamInfo[idx].is_seed = is_seed;
     },
     roundOneInfo(state, { gameLen }) {
-      let newGameInfo = Object.assign([], state.gameInfo);
-      console.log(state.gameInfo);
-      newGameInfo.unshift(
-        Array.from({ length: gameLen }, () =>
-          JSON.parse(JSON.stringify(GAME_FORM))
-        )
+      const roundOne = Array.from({ length: gameLen }, () =>
+        JSON.parse(JSON.stringify(GAME_FORM))
       );
 
       // seed
       const seedPlayer = state.teamInfo
         .filter((team) => team.is_seed)
         .map((v) => v.id);
-      const SeedCount = Math.floor(seedPlayer.length / 2);
-      let SeedIdx = 0;
+      console.log(seedPlayer);
 
       // not seed
       const notSeedPlayer = state.teamInfo
@@ -117,60 +119,75 @@ export default createStore({
         .map((v) => v.id);
       let notSeedIdx = 0;
 
+      //seed
+      const SeedCount = Math.floor(seedPlayer.length / 2);
+      let SeedIdx = 0;
+      const frontSeedCount = SeedCount + (SeedCount % 2);
+      const behindSeedCount = roundOne.length - SeedCount - 1;
+      console.log(frontSeedCount, behindSeedCount);
+
       // bye
       const byeCount = gameLen * 2 - state.teamCount;
-      if (byeCount > 0) {
-        const count = Math.floor(byeCount / 2);
-        for (const idx in newGameInfo[0]) {
-          if (
-            idx < count + (byeCount % 2) ||
-            idx >= newGameInfo[0].length - count
-          ) {
-            newGameInfo[0][idx].bye = true;
-            // play
-            if (
-              idx < SeedCount + (SeedCount % 2) ||
-              idx > newGameInfo[0].length - SeedCount - 1
-            ) {
-              console.log(SeedIdx, idx);
-              newGameInfo[0][idx].player1.id = seedPlayer[SeedIdx];
-              SeedIdx++;
-            } else {
-              newGameInfo[0][idx].player1.id = notSeedPlayer[notSeedIdx];
-              notSeedIdx++;
-            }
-          } else {
-            newGameInfo[0][idx].player1.id = notSeedPlayer[notSeedIdx];
-            newGameInfo[0][idx].player2.id = notSeedPlayer[++notSeedIdx];
-            notSeedIdx++;
+      const frontByeGameCount = Math.floor(byeCount / 2) + (byeCount % 2);
+      const behindByeGameCount = roundOne.length - Math.floor(byeCount / 2);
+      console.log(frontByeGameCount, behindByeGameCount);
+
+      for (const gameIdx in roundOne) {
+        // bye
+        if (byeCount > 0) {
+          if (gameIdx < frontByeGameCount || gameIdx >= behindByeGameCount) {
+            roundOne[gameIdx].bye = true;
           }
         }
+
+        // play: seed wrong
+        if (gameIdx < frontSeedCount || gameIdx > behindSeedCount) {
+          roundOne[gameIdx].player1.id = seedPlayer[SeedIdx];
+          SeedIdx++;
+        } else {
+          roundOne[gameIdx].player1.id = notSeedPlayer[notSeedIdx];
+          notSeedIdx++;
+        }
+
+        //
+        // if(){
+        //   roundOne[gameIdx].player1.id = notSeedPlayer[notSeedIdx];
+        //   roundOne[gameIdx].player2.id = notSeedPlayer[++notSeedIdx];
+        //   notSeedIdx++;
+        // }
       }
 
+      let newGameInfo = Object.assign([], state.gameInfo);
+      newGameInfo.unshift(roundOne);
+      state.gameInfo = newGameInfo;
+    },
+    roundOther(state, { round, gameLen }) {
+      // console.log("gameLen", gameLen);
+      console.log("round", round);
+      let newGameInfo = Object.assign([], state.gameInfo);
+
+      newGameInfo.unshift(
+        Array.from({ length: gameLen }, () =>
+          Object.assign(
+            {},
+            { ...GAME_FORM, player1: { ...GAME_FORM.player1, sort: [] } }
+          )
+        )
+      );
       state.gameInfo = newGameInfo;
       console.log(newGameInfo);
     },
-    roundOther(state, { gameLen }) {
-      console.log(gameLen);
-    },
   },
   actions: {
-    singleInfoSizeChange({ state, commit }, { base, exponent }) {
+    singleInfoSizeChange({ commit }, { base, exponent }) {
       for (let i = 0; i <= exponent; i++) {
         const gameLen = Math.pow(base, i);
-        console.log(gameLen);
-        let newGameInfo = Object.assign([], state.gameInfo);
         switch (i) {
           case exponent:
             commit("roundOneInfo", { gameLen });
             break;
           default:
-            newGameInfo.unshift(
-              Array.from({ length: gameLen }, () =>
-                Object.assign({}, GAME_FORM)
-              )
-            );
-            state.gameInfo = newGameInfo;
+            commit("roundOther", { round: exponent - i + 1, gameLen });
             break;
         }
       }
